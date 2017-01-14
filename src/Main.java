531926d7e1dbaebd7cc7ae2292575baad1b529f9
Matthew McMillan <unsolvedcypher@gmail.com>
2017-01-13 23:29:25 -0500

    added pods and got RequestProcessor to properly parse page

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		
+		Scanner scanner = new Scanner(System.in);
+		RequestProcessor processor = new RequestProcessor("pi");
+		System.out.println(processor.getResponse());
+		while (true) {
+			String input = scanner.nextLine();
+			if (processor.validRequest(input)) {
+				System.out.println(processor.podString(Integer.valueOf(input)));
+			} else {
+				processor =  new RequestProcessor(input);
+				System.out.println(processor.getResponse());
+			}
+		}
 	}
 }
--- src/RequestProcessor.java
+++ src/RequestProcessor.java
@@ -1,30 +1,71 @@
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 public class RequestProcessor {
 	private String query;
+
+	private Pod inputPod;
+	private Pod firstPod;
+	private ArrayList<Pod> extraPods = new ArrayList<>();
 	
 	public RequestProcessor(String query) {
 		this.query = query;
+		queryServer();
 	}
 	
 	/**
 	 * Queries the WolframAlpha server and returns the XML response
 	 * @return XML response
 	 */
-	private String queryServer() {
+	private void queryServer() {
 		String fullURL = "http://api.wolframalpha.com/v2/query?input=" + query + "&appid=" + ServerKey.getKey();
 		try {
-			Document doc = Jsoup.connect(fullURL).get();
+			Document doc = Jsoup.connect(fullURL).timeout(1000 * 10).get();
+			parseDocument(doc);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
-	private void parseDocument(String xml) {
-		
+	private void parseDocument(Document doc) {
+		for (Element element : doc.select("pod")) {
+			Pod currPod = new Pod(element.attr("title"), element.select("plaintext").text());
+			if (currPod.isInput()) {
+				inputPod = currPod;
+			} else if (firstPod == null) {
+				firstPod = currPod;
+			} else {
+				extraPods.add(currPod);
+			}
+		}
+	}
+
+	public String getResponse() {
+		String result = "";
+		result += inputPod == null ? "" : inputPod.toString() + "\n";
+		result += firstPod == null ? "" : firstPod.toString() + "\n";
+		if (extraPods.size() > 0) {
+			result += "Respond with a number below for more information\n";
+			for (int i = 0; i < extraPods.size(); i++) {
+				result += (i + 1) + ". " + extraPods.get(i).getTitle() + "\n";
+			}
+		}
+		return result;
+	}
+	public String podString(int podNumber) {
+		return extraPods.get(podNumber - 1).toString();
+	}
+	public boolean validRequest(String request) {
+		try {
+			int requestInt = Integer.valueOf(request);
+			return (requestInt > 0 && requestInt < extraPods.size() + 1);
+		} catch (Exception e) {
+			return false;
+		}
 	}
 }
