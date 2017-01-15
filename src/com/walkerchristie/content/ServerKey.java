27b181be2e615e7d76663ad6a5ae9d782086d607
Matthew McMillan <unsolvedcypher@gmail.com>
2017-01-14 20:31:21 -0500

    allows keys for Wolfram Alpha and Twilio

--- src/com/walkerchristie/content/ServerKey.java
+++ src/com/walkerchristie/content/ServerKey.java
@@ -4,15 +4,26 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class ServerKey {
-	private static String key;
-	
-	public static String getKey() {
+	private static String wolframKey, twilioKey;
+
+	public static String getWolframKey() {
 		try {
-			return key == null ? new String(Files.readAllBytes(Paths.get("key.txt"))) : key;
+			return wolframKey == null ? new String(Files.readAllBytes(Paths.get("wolframkey.txt"))) : wolframKey;
 		} catch (IOException e) {
-			System.out.println("Ensure that you've created a key.txt file in the root of your project.");
+			System.out.println("Ensure that you've created a wolframkey.txt file in the root of your project.");
 			e.printStackTrace();
-			
+
+			System.exit(1);
+			return null;
+		}
+	}
+	public static String getTwilioKey() {
+		try {
+			return twilioKey == null ? new String(Files.readAllBytes(Paths.get("twiliokey.txt"))) : twilioKey;
+		} catch (IOException e) {
+			System.out.println("Ensure that you've created a twiliokey.txt file in the root of your project.");
+			e.printStackTrace();
+
 			System.exit(1);
 			return null;
 		}
