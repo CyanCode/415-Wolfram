73162eb0583e9f4931be7a778fc475626d528ab6
Walker Christie <walker781@yahoo.com>
2017-01-14 21:43:08 -0500

    Edited functionality of requests which can now choose how to respond to messages

--- src/com/walkerchristie/content/RequestProcessor.java
+++ src/com/walkerchristie/content/RequestProcessor.java
@@ -7,26 +7,83 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 public class RequestProcessor {
-	private String query;
-
-	private Pod inputPod;
-	private Pod firstPod;
+	private Pod inputPod, firstPod;
+	private String currentUrl;
+	
 	private ArrayList<Pod> extraPods = new ArrayList<>();
 	private ArrayList<Assumption> assumptions = new ArrayList<>();
-	
-	public RequestProcessor(String query) {
-		this.query = query;
+
+	/**
+	 * Queries the server and returns the initial response from 
+	 * the server for the query associated with this request
+	 * @return Response from the server
+	 */
+	private String getResponse() {
+		try {
+			parseDocument(queryServer());
+		} catch (IOException e) {
+			e.printStackTrace(); // TODO Auto-generated catch block
+		}
+
+		String result = "";
+		result += inputPod == null ? "" : "\n" + inputPod.toString();
+
+		for (Assumption assumption : assumptions) {
+			result += '\n' + assumption.toString();
+		}
+
+		result += firstPod == null ? "" : "\n" + firstPod.toString();
+
+		int optionNumber = 1;
+
+		if (extraPods.size() > 0) {
+			result += "\nRespond with a number below for more information";
+			for (Pod pod : extraPods) {
+				result += "\n" + optionNumber + ". " + pod.getTitle();
+				optionNumber++;
+			}
+		}
+
+		if (assumptions.size() > 0) {
+			result += "\n" + optionNumber + ". Assumption Options";
+		}
+
+		return result.equals("") ? "" : result.substring(1);
 	}
-	
+
+	public String respondTo(String message) {
+		int choice = numberFrom(message);
+
+		if (choice > 0 && choice <= extraPods.size()) {
+			return extraPods.get(choice - 1).toString();
+		} else if (assumptions.size() > 0 && choice == extraPods.size() + 1) {
+			//TODO: Show assumptions
+		} else if (choice > assumptions.size() + 1 && choice < assumptions.size() + extraPods.size() + 1) {
+			url += "&assumption=" + assumptions.get(choice - extraPods.size() - 1).getURL(numOfAssumption);
+			return getResponse();
+		} else {
+			currentUrl = "http://api.wolframalpha.com/v2/query?input=" + message + "&appid=" + ServerKey.getWolframKey();
+			return getResponse();
+		}
+	}
+
+	public boolean validRequest(String request) {
+		try {
+			int requestInt = Integer.valueOf(request);
+			return (requestInt > 0 && requestInt < extraPods.size() + 1);
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
 	/**
 	 * Queries the WolframAlpha server and returns the XML response
 	 * @return XML response
 	 */
 	private Document queryServer() throws IOException {
-		String fullURL = "http://api.wolframalpha.com/v2/query?input=" + query + "&appid=" + ServerKey.getWolframKey();
-		return Jsoup.connect(fullURL).timeout(1000 * 10).get();
+		return Jsoup.connect(currentUrl).timeout(1000 * 10).get();
 	}
-	
+
 	private void parseDocument(Document doc) {
 		for (Element element : doc.select("pod")) {
 			Pod currPod = new Pod(element.attr("title"), element.select("plaintext").text());
@@ -38,60 +95,21 @@ public class RequestProcessor {
 				extraPods.add(currPod);
 			}
 		}
-		
+
 		for (Element element : doc.select("assumption")) {
 			assumptions.add(new Assumption(element));
 		}
-
 	}
 
-	/**
-	 * Queries the server and returns the initial response from 
-	 * the server for the query associated with this request
-	 * @return Response from the server
-	 */
-	public String getResponse() {
+	private int numberFrom(String num) {
 		try {
-			parseDocument(queryServer());
-		} catch (IOException e) {
-			e.printStackTrace(); // TODO Auto-generated catch block
-		}
-		
-		String result = "";
-		result += inputPod == null ? "" : "\n" + inputPod.toString();
-		
-		for (Assumption assumption : assumptions) {
-			result += '\n' + assumption.toString();
-		}
-		result += firstPod == null ? "" : "\n" + firstPod.toString();
-		
-		int optionNumber = 1;
-		
-		if (extraPods.size() > 0) {
-			result += "\nRespond with a number below for more information";
-			for (Pod pod : extraPods) {
-				result += "\n" + optionNumber + ". " + pod.getTitle();
-				optionNumber++;
-			}
-		}
-		
-		if (assumptions.size() > 0) {
-			result += "\n" + optionNumber + ". Assumption Options";
-		}
-		
-		return result.equals("") ? "" : result.substring(1);
-	}
-	
-	public String podString(int podNumber) {
-		return extraPods.get(podNumber - 1).toString();
-	}
-	
-	public boolean validRequest(String request) {
-		try {
-			int requestInt = Integer.valueOf(request);
-			return (requestInt > 0 && requestInt < extraPods.size() + 1);
+			return Integer.parseInt(num);
 		} catch (Exception e) {
-			return false;
+			try {
+				return Integer.parseInt(num.substring(0, num.length() - 1));
+			} catch (Exception e2) {
+				return -1;
+			}
 		}
 	}
 }
--- src/com/walkerchristie/content/SMSManager.java
+++ src/com/walkerchristie/content/SMSManager.java
@@ -1,9 +1,9 @@
 package com.walkerchristie.content;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,6 +14,7 @@ import com.twilio.twiml.MessagingResponse;
 import com.twilio.twiml.TwiMLException;
 
 public class SMSManager extends HttpServlet  {
+	private HashMap<String, RequestProcessor> sessions = new HashMap<>();
 	private static final long serialVersionUID = 6521584851387748583L;
 
 	@Override
@@ -34,11 +35,26 @@ public class SMSManager extends HttpServlet  {
 
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String phoneNum = request.getParameter("From");
+		String message = request.getParameter("Body");
+		RequestProcessor currentSession;
+		
+		if (sessions.containsKey(phoneNum)) {
+			currentSession = sessions.get(phoneNum);
+		} else {
+			currentSession = new RequestProcessor(message);
+		}
+		
+		
+		
 		Message sms = new Message
 				.Builder()
 				.body(new Body("The Robots are coming! Head for the hills!"))
 				.build();
 
+		String num = request.getParameter("From");
+		String body = request.getParameter("Body");
+		
 		System.out.println("TEST");
 		MessagingResponse twiml = new MessagingResponse.Builder().message(sms).build();
 		response.setContentType("application/xml");
